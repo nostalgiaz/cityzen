868bd5cf3841f3d435eba0da424915061ec72290
mattia <mattia@ahref.eu>
2012-12-02 12:32:17 +0100

    remove markers from map if not needed

--- cityzen/main/static/js/cityzen.js
+++ cityzen/main/static/js/cityzen.js
@@ -1,4 +1,5 @@
 $(function () {
+  window.marker = [];
   addPointToMap = function (address, category, myStatus) {
     geocoder.geocode(
       {
@@ -12,12 +13,12 @@ $(function () {
             if (myStatus === 0)
               image = 'http://www.gettyicons.com/free-icons/218/mixed/png/32/blue_pin_32.png';
             else image = 'http://www.gettyicons.com/free-icons/218/mixed/png/32/green_pin_32.png'
-          else if (category === 1)
-            if (myStatus === 0)
-              image = 'http://www.gettyicons.com/free-icons/218/mixed/png/32/red_pin_32.png';
-            else image = 'http://www.gettyicons.com/free-icons/218/mixed/png/32/black_pin_32.png'
+            else if (category === 1)
+              if (myStatus === 0)
+                image = 'http://www.gettyicons.com/free-icons/218/mixed/png/32/red_pin_32.png';
+              else image = 'http://www.gettyicons.com/free-icons/218/mixed/png/32/black_pin_32.png'
 
-          new google.maps.Marker({
+          window.marker.push(new google.maps.Marker({
             position: results[0].geometry.location,
             map: map,
             icon: new google.maps.MarkerImage(
@@ -26,7 +27,7 @@ $(function () {
               new google.maps.Point(0, 0),
               new google.maps.Point(0, 32)
             )
-          });
+          }));
           map.setCenter(results[0].geometry.location);
         }
         else {
@@ -36,7 +37,7 @@ $(function () {
     );
   };
 
-  var map = new google.maps.Map(document.getElementById('map'), {
+  window.map = new google.maps.Map(document.getElementById('map'), {
     mapTypeId: google.maps.MapTypeId.TERRAIN,
     zoom: 12,
     panControl: false,
@@ -58,8 +59,7 @@ $(function () {
       address: ''
     },
     initialize: function () {
-      var attr = this.attributes;
-      addPointToMap(attr.address, attr.category, attr.status);
+
     }
   });
 
@@ -95,10 +95,12 @@ $(function () {
       this.ticket.fetch({success: function () {
         self.render();
       }});
-      this.ticket.bind('update-status', function(data) {
-        $.post('/api/tickets/status/update/', data, function (response) {
-          console.log(response);
-        })
+      this.ticket.bind('update-status', function (data) {
+        $.post('/api/tickets/status/update/', data, function () {
+          self.ticket.fetch({success: function () {
+            self.render();
+          }});
+        });
       });
     },
     render: function () {
@@ -107,14 +109,23 @@ $(function () {
 
       $el.empty();
 
-      _.each(tickets, function (ticket, i) {
-        var $element = new TicketView({model: ticket}).render().el;
+      if (window.marker && window.marker.length !== 0) {
+        for (var i = 0; i < window.marker.length; ++i) {
+          window.marker[i].setMap(null);
+        }
+      }
+
+      _.each(tickets, function (ticket) {
+        var attr = ticket.attributes
+          , $element = new TicketView({model: ticket}).render().el;
         $el.prepend($element);
+
+        addPointToMap(attr.address, attr.category, attr.status);
       }, this);
 
       $('.btn').on('click', function () {
         var $wrapper = $(this).closest('div.row-fluid');
-          window.tickets.trigger('update-status', {'pk': $wrapper.data('pk'), 'status': $wrapper.data('status')});
+        window.tickets.trigger('update-status', {'pk': $wrapper.data('pk'), 'status': $wrapper.data('status')});
       });
 
       return this;
